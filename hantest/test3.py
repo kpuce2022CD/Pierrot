dd17f39f5e308b0390816381f9ac674de3563228
han <dbn0224@gmail.com>
2022-01-13 14:40:36 +0900

    선수 트랙킹 두 선수 트랙킹 가능
    예정) 각 두 선수 위치 저장 및 함수로 코드 다시 작성

--- hantest/test3.py
+++ hantest/test3.py
@@ -1,7 +1,8 @@
 # Tracker APIs (track_trackingAPI.py)
 import collections
-
 import cv2
+
+
 
 # 트랙커 객체 생성자 함수 리스트 ---①
 trackers = cv2.TrackerCSRT_create
@@ -20,41 +21,40 @@ Point = collections.namedtuple('Point', ['x', 'y'])
 
 while cap.isOpened():
     ret, frame = cap.read()
-    #  print("frame : ",frame)
     if not ret:
         print('Cannot read video file')
         break
     img_draw = frame.copy()
     if tracker is None:  # 트랙커 생성 안된 경우
-        cv2.putText(img_draw, "Press the Space to set ROI!!", \
-                    (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(img_draw, "Press the Space to set ROI!!", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
     else:
         ok, bbox = tracker.update(frame)  # 새로운 프레임에서 추적 위치 찾기 ---③
         ok1, bbox1 = tracker1.update(frame)  # 새로운 프레임에서 추적 위치 찾기 ---③
         (x, y, w, h) = bbox
         (x1, y1, w1, h1) = bbox1
+        print("bbox",bbox)
+        print("bbox1",bbox1)
         # 선수 좌표 저장
-        player_coord = Point(x=int(x+w/2), y=int(y+h/2))
-        cv2.circle(img_draw,player_coord,10,(0,0,255),-1)
+        player_coord1 = Point(x=int(x+w/2), y=int(y+h/2))
+        cv2.circle(img_draw,player_coord1,10,(0,0,255),-1)
 
         if ok:  # 추적 성공
             cv2.rectangle(img_draw, (int(x), int(y)), (int(x + w), int(y + h)), \
                           (0, 255, 0), 2, 1)
-            print("ok 추적성공")
         else:  # 추적 실패
             cv2.putText(img_draw, "Tracking fail.", (100, 80), \
                         cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
         if ok1:  # 추적 성공
-            print("ok1 추적성공")
             cv2.rectangle(img_draw, (int(x1), int(y1)), (int(x1 + w1), int(y1 + h1)), \
                           (0, 255, 255), 2, 1)
         else:  # 추적 실패
             cv2.putText(img_draw, "Tracking fail.", (100, 80), \
                         cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
 
-    trackerName = tracker.__class__.__name__
-    cv2.putText(img_draw, ":" + trackerName, (100, 20), \
-                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
+    # trackerName = tracker.__class__.__name__
+    # cv2.putText(img_draw, ":" + trackerName, (100, 20), \
+    #             cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(img_draw,"Player1",(100,80), cv2.FONT_HERSHEY_SIMPLEX,0.75,(0,0,255),2,cv2.LINE_AA)
 
     cv2.imshow(win_name, img_draw)
     key = cv2.waitKey(delay) & 0xff
@@ -64,17 +64,18 @@ while cap.isOpened():
         print("roi 전")
         roi = cv2.selectROI(win_name, frame, False)  # 초기 객체 위치 설정
         print("roi 후")
-        roi1 = cv2.selectROI(win_name,frame,False)
-        print("roi 후")
         print("roi", roi)
         if roi[2] and roi[3]:  # 위치 설정 값 있는 경우
             print("roi[2] and roi[3]")
             tracker = trackers()  # 트랙커 객체 생성 ---⑤
             isInit = tracker.init(frame, roi)
+
+        print("pause")
+        roi1 = cv2.selectROI(win_name,frame,False)
         if roi1[2] and roi1[3]:  # 위치 설정 값 있는 경우
             print("roi[2] and roi[3]")
             tracker1 = trackers()  # 트랙커 객체 생성 ---⑤
-            isInit = tracker1.init(frame, roi)
+            isInit = tracker1.init(frame, roi1)
     elif key == 27:
         break
 else:
