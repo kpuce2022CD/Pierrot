9a31f2515fd692de8b357e995832dbb965bd786c
han <dbn0224@gmail.com>
2022-01-14 18:03:49 +0900

    두 선수 좌표 저장 완료, 함수화 완료 트랙킹 놓쳤을 경우 테스트 필요

---
+++ hantest/main.py
@@ -0,0 +1,4 @@
+from player import Player
+
+player = Player("input_video.mp4", "win")
+player.play_video()
--- hantest/player.py
+++ hantest/player.py
@@ -4,40 +4,91 @@ import collections
 import cv2
 
 
+# 트랙킹놓쳤을 때 정지 시킨 후 다시 잡기, main 에서 실행시키기
+
 # video, win name 입력
 class Player:
+    player = None
+    rival = None
+    isFirst = True
+    player_coord_text = open('player_coord.txt', 'w')
+    rival_coord_text = open('rival_coord.txt', 'w')
+    player_coord_string = ''
+    rival_coord_string = ''
+
     def __init__(self, video_src, win_name):
         self.tracker = cv2.TrackerCSRT_create
-        self.player = None
-        self.rival = None
         self.video_src = video_src  # video file
         self.cap = cv2.VideoCapture(self.video_src)
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
         self.win_name = win_name
-        self.Point = collections.namedtuple('Point', ['x', 'y'])  # 좌표 저장하기위한 collection
-        self.isFirst = True
 
     def play_video(self):
         if self.cap.isOpened():
+            player_coord_text = open('player_coord.txt', 'w')
+            rival_coord_text = open('rival_coord.txt', 'w')
+            player_coord_string = ''
+            rival_coord_string = ''
             while True:
                 ret, frame = self.cap.read()
                 if not ret:
                     print("finish")
+                    player_coord_text.write(player_coord_string)
+                    player_coord_text.close()
+                    rival_coord_text.write(rival_coord_string)
+                    rival_coord_text.close()
+                    self.cap.release()
                     return
-                if self.isFirst:
-                    self.tracking(frame)
+                # 처음 실행시켰을 경우 두 선수를 잡아주어야 하기에
                 img_draw = frame.copy()
+                key = cv2.waitKey(1) & 0xff
+                if key == ord(' ') or self.isFirst:
+                    self.isFirst = False
+                    self.tracking(frame)
+                # 트랙커가 생성되지 않은 경우
+                if self.player is None or self.rival is None:
+                    self.tracking(frame)
+                else:
+                    player_ok, player_bbox = self.player.update(frame)
+                    rival_ok, rival_bbox = self.rival.update(frame)
+                    Point = collections.namedtuple('Point', ['x', 'y'])  # 좌표 저장하기위한 collection
+                    (player_x, player_y, player_w, player_h) = player_bbox
+                    (rival_x, rival_y, rival_w, rival_h) = rival_bbox
+
+                    # 두 선수 좌표 저장
+                    player_coord = Point(x=int(player_x + player_w / 2), y=int(player_y + player_h / 2))
+                    rival_coord = Point(x=int(rival_x + rival_w / 2), y=int(rival_y + rival_h / 2))
+                    player_coord_string = player_coord_string + str(player_coord.x) + ',' + str(player_coord.y) + '\n'
+                    rival_coord_string = rival_coord_string + str(rival_coord.x) + ',' + str(rival_coord.y) + '\n'
+                    print("player_coord_string: ", player_coord_string)
+                    # print("player_coord.x",type(player_coord.x))
+
+                    # 추적 성공
+                    if player_ok and rival_ok:
+                        cv2.rectangle(img_draw, (int(player_x), int(player_y)),
+                                      (int(player_x + player_w), int(player_y + player_h)), (0, 255, 0), 2, 1)
+                        cv2.rectangle(img_draw, (int(rival_x), int(rival_y)),
+                                      (int(rival_x + rival_w), int(rival_y + rival_h)), (0, 255, 255), 2, 1)
+                        cv2.circle(img_draw, player_coord, 10, (0, 255, 0), -1)
+                        cv2.circle(img_draw, rival_coord, 10, (0, 255, 255), -1)
+                    # 추적 실패
+                    else:
+                        self.tracker(frame)
+                cv2.imshow(self.win_name, img_draw)
         else:
             print('Cannot read video file')
+            self.cap.release()
 
     # 선수, 상대 선수 트랙킹
     def tracking(self, frame):
         # 선수
         player_roi = cv2.selectROI(self.win_name, frame, False)
-        self.player = self.tracker()
-        self.player.init(frame,player_roi)
+        if player_roi[2] and player_roi[3]:
+            self.player = self.tracker()
+            isInit = self.player.init(frame, player_roi)
 
         # 상대 선수
-        rival_roi = cv2.selectROI(self.win_name,frame,False)
-        self.rival = self.tracker()
-        self.rival.init(frame,rival_roi)
+        rival_roi = cv2.selectROI(self.win_name, frame, False)
+        if rival_roi[2] and rival_roi[3]:
+            self.rival = self.tracker()
+            isInit = self.rival.init(frame, rival_roi)
