66c7bc5963fda0b5320a2f3fa801a43ec6fcaa94
1ilkwon <dlfrnjs318@gmail.com>
2022-01-21 17:03:11 +0900

    코트 픽셀 수정
    filter_pixels 함수 추가

--- ilkwon_track/court_dection.py
+++ ilkwon_track/court_dection.py
@@ -45,3 +45,28 @@ class CourtDetector:
         self.best_conf = None
         self.frame_points = None
         self.dist = 5
+        
+        
+        #기본적으로 화이트 구별하기 위한 함수
+        def _threshold(self, frame):
+            """
+        간단한 thresholding으로 화이트 픽셀 구현
+        """
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1] #여기서 왜 200으로 하는지 모르겠음 실행하면서 눈으로 봐야할것같음
+        return gray
+    
+        def _filter_pixels(self, gray):
+            """
+        코트 라인 구조를 사용해서 픽셀을 수정
+        """
+        for i in range(self.dist_tau, len(gray) - self.dist_tau): #tau : 임계값, range(시작숫자,종료숫자)
+            for j in range(self.dist_tau, len(gray[0]) - self.dist_tau):
+                if gray[i, j] == 0:
+                    continue
+                if (gray[i, j] - gray[i + self.dist_tau, j] > self.intensity_threshold and gray[i, j] - gray[i - self.dist_tau, j] > self.intensity_threshold):
+                    continue
+                if (gray[i, j] - gray[i, j + self.dist_tau] > self.intensity_threshold and gray[i, j] - gray[i, j - self.dist_tau] > self.intensity_threshold):
+                    continue
+                gray[i, j] = 0
+        return gray
