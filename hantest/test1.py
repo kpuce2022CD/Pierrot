c65de97f01b7524af44058002b2e42fb6784550a
han <dbn0224@gmail.com>
2022-01-26 10:55:21 +0900

    1ilkwon, HAN 코드 병합

--- hantest/test1.py
+++
@@ -1,9 +0,0 @@
-# ball tracking test
-
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-while True:
-
-    ret,img_color = cap.read()
--- ilkwon_track/play.py
+++
@@ -1,4 +0,0 @@
-from court_detector import CourtDetector
-
-CourtDetector = CourtDetector("input_video.mp4", "win")
-CourtDetector.play_video()
