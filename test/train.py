b20c4bbed31bf2dac6c72688b2274069b64d7fbd
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-02-11 18:26:18 +0900

    .

--- test/train.py
+++ test/train.py
@@ -1,29 +1,24 @@
 import cv2
 import queue
 import numpy
+import pandas
+import pickle
+import time
+
+from sktime.datatypes._panel._convert import from_2d_array_to_nested
 from PIL import Image, ImageDraw
-from matplotlib import pyplot
+from pickle import load
 from tracknet import trackNet
-import os
-import tensorflow as tf
+from detection import *
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    # 텐서플로가 세 번째 GPU만 사용하도록 제한
-    try:
-        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-    except RuntimeError as e:
-        # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
-        print(e)
-
-
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # 변수정의
 current_frame = 0
 tracknet_width, tracknet_height = 640, 360
-
+bounce = 1
+output_video_path = 'video_output.mp4'
+coords = []
+check_time = []
 
 # 영상불러오기 및 영상정보 추출
 video = cv2.VideoCapture('video_cut.mp4')
@@ -52,17 +47,19 @@ for i in range(0, 8):
 # 영상 저장을 위한 셋팅
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 output_video = cv2.VideoWriter(
-    'video_output.mp4', fourcc, fps, (frame_width, frame_height))
+    output_video_path, fourcc, fps, (frame_width, frame_height))
 
+last = time.time()  # start counting
 # 프레임단위로 반복
 while True:
-    print('percentage of video processed : {}%'.format(
-        round((current_frame/num_frames)*100)))
+    print('percentage of video processed : {}%, number of frames : {}frame'.format(
+        round((current_frame/num_frames)*100), current_frame))
 
     # 프레임단위로 읽기
     # video.set(1, current_frame)
     ret, frame = video.read()
 
+    # 다음 프레임이 없으면 끝
     if not ret:
         break
 
@@ -98,16 +95,21 @@ while True:
         if len(circles) == 1:
             x = int(circles[0][0][0])
             y = int(circles[0][0][1])
-
+            coords.append([x, y])
+            check_time.append(time.time()-last)
             trajectory_ball.appendleft([x, y])
             trajectory_ball.pop()
 
         # 두 개 이상일 시 트래킹 표시하지 않음
         else:
+            coords.append(None)
+            check_time.append(time.time()-last)
             trajectory_ball.appendleft(None)
             trajectory_ball.pop()
     # 공 후보 트래킹 실패 시 트래킹 표시하지 않음
     else:
+        coords.append(None)
+        check_time.append(time.time()-last)
         trajectory_ball.appendleft(None)
         trajectory_ball.pop()
 
@@ -128,10 +130,117 @@ while True:
     output_video.write(opencvImage)
     current_frame += 1
 
-    # cv2.imshow("Video", frame)
-    # if cv2.waitKey(1) & 0xFF == 27:
-    #     break
+video.release()
+output_video.release()
+
+# 전 프레임의 공과 위치 비교 및 이상값제거
+for _ in range(3):
+    x, y = diff_xy(coords)
+    remove_outliers(x, y, coords)
+
+# 보간법. 트래킹이 안되었을 시 예측값삽입
+coords = interpolation(coords)
+
+# velocity
+Vx = []
+Vy = []
+V = []
+frames = [*range(len(coords))]
+print("frames : {}".format(frames))
+
+for i in range(len(coords)-1):
+    p1 = coords[i]
+    p2 = coords[i+1]
+    t1 = check_time[i]
+    t2 = check_time[i+1]
+    x = (p1[0]-p2[0])/(t1-t2)
+    y = (p1[1]-p2[1])/(t1-t2)
+    Vx.append(x)
+    Vy.append(y)
+
+for i in range(len(Vx)):
+    vx = Vx[i]
+    vy = Vy[i]
+    v = (vx**2+vy**2)**0.5
+    V.append(v)
+
+xy = coords[:]
+
+if bounce == 1:
+    # Predicting Bounces
+    test_df = pandas.DataFrame(
+        {'x': [coord[0] for coord in xy[:-1]], 'y': [coord[1] for coord in xy[:-1]], 'V': V})
+
+    # df.shift
+    for i in range(20, 0, -1):
+        test_df[f'lagX_{i}'] = test_df['x'].shift(i, fill_value=0)
+    for i in range(20, 0, -1):
+        test_df[f'lagY_{i}'] = test_df['y'].shift(i, fill_value=0)
+    for i in range(20, 0, -1):
+        test_df[f'lagV_{i}'] = test_df['V'].shift(i, fill_value=0)
+
+    test_df.drop(['x', 'y', 'V'], 1, inplace=True)
+
+    Xs = test_df[['lagX_20', 'lagX_19', 'lagX_18', 'lagX_17', 'lagX_16',
+                  'lagX_15', 'lagX_14', 'lagX_13', 'lagX_12', 'lagX_11', 'lagX_10',
+                  'lagX_9', 'lagX_8', 'lagX_7', 'lagX_6', 'lagX_5', 'lagX_4', 'lagX_3',
+                  'lagX_2', 'lagX_1']]
+    Xs = from_2d_array_to_nested(Xs.to_numpy())
+
+    Ys = test_df[['lagY_20', 'lagY_19', 'lagY_18', 'lagY_17',
+                  'lagY_16', 'lagY_15', 'lagY_14', 'lagY_13', 'lagY_12', 'lagY_11',
+                  'lagY_10', 'lagY_9', 'lagY_8', 'lagY_7', 'lagY_6', 'lagY_5', 'lagY_4',
+                  'lagY_3', 'lagY_2', 'lagY_1']]
+    Ys = from_2d_array_to_nested(Ys.to_numpy())
+
+    Vs = test_df[['lagV_20', 'lagV_19', 'lagV_18',
+                  'lagV_17', 'lagV_16', 'lagV_15', 'lagV_14', 'lagV_13', 'lagV_12',
+                  'lagV_11', 'lagV_10', 'lagV_9', 'lagV_8', 'lagV_7', 'lagV_6', 'lagV_5',
+                  'lagV_4', 'lagV_3', 'lagV_2', 'lagV_1']]
+    Vs = from_2d_array_to_nested(Vs.to_numpy())
+
+    X = pandas.concat([Xs, Ys, Vs], 1)
+
+    # load the pre-trained classifier
+    clf = load(open('clf.pkl', 'rb'))
+
+    predcted = clf.predict(X)
+    idx = list(numpy.where(predcted == 1)[0])
+    idx = numpy.array(idx) - 10
+
+#   if minimap == 1:
+#         video = cv2.VideoCapture('VideoOutput/video_with_map.mp4')
+#   else:
+    video = cv2.VideoCapture(output_video_path)
+
+    output_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    output_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    print('fps : {}'.format(fps))
+    print('frame sizee : {}x{}'.format(output_width, output_height))
+    print('num_frames :{}'.format(num_frames))
+
+    output_video = cv2.VideoWriter(
+        'final_video.mp4', fourcc, fps, (output_width, output_height))
+    i = 0
+    while True:
+        ret, frame = video.read()
+        if ret:
+          # if coords[i] is not None:
+            if i in idx:
+                center_coordinates = int(xy[i][0]), int(xy[i][1])
+                radius = 3
+                color = (255, 0, 0)
+                thickness = -1
+                cv2.circle(frame, center_coordinates, 10, color, thickness)
+            i += 1
+            output_video.write(frame)
+        else:
+            break
+
 
 video.release()
 output_video.release()
-cv2.destroyAllWindows
