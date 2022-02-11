d3be99886d539ec21e2646c78a185564aa32294c
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-02-12 00:37:48 +0900

    create table error

---
+++ web/tennisanalysis/src/main/kotlin/com/pierrot/tennisanalysis/repository/Member.kt
@@ -0,0 +1,18 @@
+package com.pierrot.tennisanalysis.repository
+
+import org.hibernate.annotations.ColumnDefault
+import javax.persistence.*
+
+@Entity
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    @ColumnDefault(value = "false")
+    var completed: Boolean = false,
+
+    @Column(nullable = false)
+    var todoName: String
+)
