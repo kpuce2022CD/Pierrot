3ff6d6eefafa45c32b632ec5f724ac84923b2a4a
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-02-11 21:06:10 +0900

    .

---
+++ web/tennisball_login/tennisanalysis/src/main/kotlin/com/pierrot/tennisanalysis/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.pierrot.tennisanalysis
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(TennisanalysisApplication::class.java)
+	}
+
+}
---
+++ web/tennisball_login/tennisanalysis/src/main/kotlin/com/pierrot/tennisanalysis/TennisanalysisApplication.kt
@@ -0,0 +1,11 @@
+package com.pierrot.tennisanalysis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TennisanalysisApplication
+
+fun main(args: Array<String>) {
+	runApplication<TennisanalysisApplication>(*args)
+}
