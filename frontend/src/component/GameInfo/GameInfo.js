3812674caa22d1461df95e95e78e10ffb24de5db
han0224 <70616579+han0224@users.noreply.github.com>
2022-02-12 11:32:14 +0900

    경기 리스트 화면에 출력 -> db 연동후 다시 테스트

--- frontend/src/component/GameInfo/GameInfo.js
+++ frontend/src/component/GameInfo/GameInfo.js
@@ -1,5 +1,178 @@
+import "./GameInfo.css"
+import Layout from "../../Layout/Layout";
+
 function GameInfo() {
-  return <div>게임 정보</div>;
+
+  const Game = ({game}) =>{
+    return(
+      <div className="gamelist">
+        {game.title}
+        {game.day}
+      </div>
+    )
+  }
+
+  // db 연동 전 테스트
+  const games = [
+    {
+      id: 0,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-10',
+    },
+    {
+      id: 1,
+      title: '정왕동',
+      player: 'lee',
+      day: '2022-02-11',
+    },
+    {
+      id: 2,
+      title: '정왕동',
+      player: 'park',
+      day: '2022-02-12',
+    },
+    {
+      id: 3,
+      title: '정왕동',
+      player: 'han',
+      day: '2022-02-13',
+    },
+    {
+      id: 4,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-14',
+    },
+    {
+      id: 0,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-10',
+    },
+    {
+      id: 1,
+      title: '정왕동',
+      player: 'lee',
+      day: '2022-02-11',
+    },
+    {
+      id: 2,
+      title: '정왕동',
+      player: 'park',
+      day: '2022-02-12',
+    },
+    {
+      id: 3,
+      title: '정왕동',
+      player: 'han',
+      day: '2022-02-13',
+    },
+    {
+      id: 4,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-14',
+    },
+    {
+      id: 0,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-10',
+    },
+    {
+      id: 1,
+      title: '정왕동',
+      player: 'lee',
+      day: '2022-02-11',
+    },
+    {
+      id: 2,
+      title: '정왕동',
+      player: 'park',
+      day: '2022-02-12',
+    },
+    {
+      id: 3,
+      title: '정왕동',
+      player: 'han',
+      day: '2022-02-13',
+    },
+    {
+      id: 4,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-14',
+    },
+    {
+      id: 0,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-10',
+    },
+    {
+      id: 1,
+      title: '정왕동',
+      player: 'lee',
+      day: '2022-02-11',
+    },
+    {
+      id: 2,
+      title: '정왕동',
+      player: 'park',
+      day: '2022-02-12',
+    },
+    {
+      id: 3,
+      title: '정왕동',
+      player: 'han',
+      day: '2022-02-13',
+    },
+    {
+      id: 4,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-14',
+    },
+    {
+      id: 0,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-10',
+    },
+    {
+      id: 1,
+      title: '정왕동',
+      player: 'lee',
+      day: '2022-02-11',
+    },
+    {
+      id: 2,
+      title: '정왕동',
+      player: 'park',
+      day: '2022-02-12',
+    },
+    {
+      id: 3,
+      title: '정왕동',
+      player: 'han',
+      day: '2022-02-13',
+    },
+    {
+      id: 4,
+      title: '정왕동',
+      player: 'kim',
+      day: '2022-02-14',
+    },
+  ]
+
+  return <div>
+    <Layout>
+    {games.map(game => (
+      <Game game={game}/>))}
+    </Layout>
+   
+  </div>;
 }
 
 export default GameInfo;
