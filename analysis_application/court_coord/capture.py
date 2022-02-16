308a95563a5203b8f6745c49caf223dc79828ca7
1ilkwon <dlfrnjs318@gmail.com>
2022-02-16 14:25:04 +0900

    court_coord 폴더
    동영상 캡쳐 후 이미지 파일 생성과 이미지 파일 마우스 좌표 출력

---
+++ analysis_application/court_coord/capture.py
@@ -0,0 +1,18 @@
+import cv2
+
+# 영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
+vidcap = cv2.VideoCapture('output_top_view.avi')
+
+count = 0
+
+while (vidcap.isOpened()):
+    ret, image = vidcap.read()
+    if (int(vidcap.get(1)) % 400 == 0):
+        print('Saved frame number : ' + str(int(vidcap.get(1))))
+        cv2.imwrite("./capture_image/frame%d.jpg" % count, image)
+        print('Saved frame%d.jpg' % count)
+        count += 1
+
+
+vidcap.release()
+
