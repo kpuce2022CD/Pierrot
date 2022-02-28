00d8eb13087397bdebc7ea400ba790010f80e7bb
han0224 <70616579+han0224@users.noreply.github.com>
2022-02-28 18:15:21 +0900

    1번째 선수 히트맵 표시

--- frontend/src/component/Graph/Graph.js
+++ frontend/src/component/Graph/Graph.js
@@ -5,40 +5,41 @@ import Layout from "../../Layout/Layout";
 import "./Graph.css";
 
 const Graph = () => {
-  // now generate some random data
-  let points = [];
-  let max = 0;
-  let width = 450;
-  let height = 271;
-  let len = 200;
-
-  while (len--) {
-    let val = Math.floor(Math.random() * 100);
-    max = Math.max(max, val);
-    let point = {
-      x: Math.floor(Math.random() * width),
-      y: Math.floor(Math.random() * height),
-      value: val,
+  // 추출한 데이터의 x, y좌표와 그려줄 x, y좌표 변환 필요
+  // 추출했을 당시 세로, 그려줄 좌표는 가로
+  // width: 450, height: 271
+  const points = [];
+  const jsonData = require("../../tempData/playerCoords.json");
+  // console.log(jsonData);
+  jsonData.forEach((data) => {
+    const point = {
+      x: parseInt(data.y_0),
+      y: parseInt(data.x_0),
+      value: 1,
     };
     points.push(point);
-  }
-  // heatmap data format
-  let data = {
+  });
+
+  const max = 50;
+  const data = {
     max: max,
     data: points,
   };
+
   useEffect(() => {
     let heatmapInstance = h337.create({
       container: document.querySelector(".heatmap"),
-      opacity: "0.9",
+      opacity: "0.3",
     });
     console.log(data);
+    console.log(heatmapInstance);
     heatmapInstance.setData(data);
   }, []);
+
   return (
     <div className="grapy">
       <Layout>
-        <div className="heatmap">ddddddddddd</div>
+        <div className="heatmap"></div>
       </Layout>
     </div>
   );
