31a84ac9bb00fe89f2c3e95091c490debca90018
han0224 <dbn0224@gmail.com>
2022-04-15 22:37:53 +0900

    react-chartjs-2 적용

--- web/frontend/src/component/Graph/Graph.js
+++ web/frontend/src/component/Graph/Graph.js
@@ -3,6 +3,9 @@ import "heatmap.js";
 import React, { Component, useState, useEffect } from "react";
 import Layout from "../../Layout/Layout";
 import "./Graph.css";
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Chart, Bubble, Line } from 'react-chartjs-2'
+ChartJS.register(...registerables);
 
 const Graph = () => {
   // 추출한 데이터의 x, y좌표와 그려줄 x, y좌표 변환 필요
@@ -10,7 +13,8 @@ const Graph = () => {
   // width: 450, height: 271
   const points = [];
   const jsonData = require("../../tempData/playerCoords.json");
-  // console.log(jsonData);
+  // console.log(jsonData[0]);
+  // console.log(typeof jsonData[0]);
   jsonData.forEach((data) => {
     const point = {
       x: parseInt(data.y_0),
@@ -21,25 +25,92 @@ const Graph = () => {
   });
 
   const max = 50;
-  const data = {
+  const playerData = {
     max: max,
     data: points,
   };
 
+  const point2 =[];
+
+  const bounceJsonData = require("../../tempData/bounces.json");
+  bounceJsonData.forEach(data=>{
+    const point = {
+      x: parseInt(data.x),
+      y: parseInt(data.y),
+      value:10,
+    };
+    point2.push(point);
+  })
+
+  const bounceData = {
+    max: max,
+    data: point2,
+  }
+
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        type: 'line',
+        label: 'Dataset 1',
+        borderColor: 'rgb(54, 162, 235)',
+        borderWidth: 2,
+        data: [1, 2, 3, 4, 5],
+      },
+      {
+        type: 'bar',
+        label: 'Dataset 2',
+        backgroundColor: 'rgb(255, 99, 132)',
+        data: [1, 2, 3, 4, 5, 6],
+        borderColor: 'red',
+        borderWidth: 2,
+      },
+      {
+        type: 'bar',
+        label: 'Dataset 3',
+        backgroundColor: 'rgb(75, 192, 192)',
+        data: [1, 2, 3, 4, 5, 6],
+      },
+    ],
+  };
+
+  const charData = {
+    datasets:[
+      {
+        type: 'bubble',
+        data: bounceData,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Bubble Chart'
+            }
+          }
+        },
+      }
+    ]
+  }
+
   useEffect(() => {
-    const heatmapInstance = h337.create({
-      container: document.querySelector(".heatmap"),
+    const playerHeatmapInstance = h337.create({
+      container: document.querySelector(".player-heatmap"),
       opacity: "0.3",
     });
-    console.log(data);
-    console.log(heatmapInstance);
-    heatmapInstance.setData(data);
+    // console.log(playerData);
+    // console.log(playerHeatmapInstance);
+    playerHeatmapInstance.setData(playerData);
+
   }, []);
 
   return (
     <Layout>
       <div className="graph">
-        <div className="heatmap"></div>
+        <div className="player-heatmap"></div>
         <div className="graph-info">
           <div>
             <h2>총 이동 거리</h2>
@@ -49,7 +120,9 @@ const Graph = () => {
             <h2>총 000</h2>
             <p>000</p>
           </div>
-          
+        </div>
+        <div className="bounce">
+          <Bubble type='line' data = {data}/>
         </div>
       </div>
     </Layout>
