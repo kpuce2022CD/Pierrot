473938bc12f0f5d78db51f5e453e94ef8d4c95e0
han0224 <dbn0224@gmail.com>
2022-05-31 21:24:00 +0900

    backend: login, signup, logout, auth frontend: sidebar 수정 중

--- web/frontend/src/Layout/Sidebar.js
+++ web/frontend/src/Layout/Sidebar.js
@@ -1,28 +1,86 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import React, { Component } from "react";
 import SidebarItem from "./SidebarItem";
 import "./Layout.css";
+import { logout } from "../apis";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const clicklogout = async (e) => {
+    console.log(e);
+    const res = await logout();
+    if (res.success) {
+      // navigate()
+    }
+  };
+  const clickitem = (e) => {
+    console.log(e);
+  };
   const menus = [
-    { name: "Main", path: "/main" },
-    { name: "Profile", path: "/playerinfo" },
-    { name: "game", path: "/gameinfo" },
-    { name: "upload", path: "/videoupload" },
-    { name: "Guide", path: "/guide" },
-    { name: "logout", path: "/" },
+    {
+      name: "Main",
+      path: "/main",
+      fun: function () {
+        navigate("/main");
+      },
+    },
+    {
+      name: "Profile",
+      path: "/playerinfo",
+      fun: function () {
+        console.log(this.path);
+        navigate("/main");
+      },
+    },
+    {
+      name: "game",
+      path: "/gameinfo",
+      fun: function () {
+        console.log(this.path);
+        navigate("/main");
+      },
+    },
+    {
+      name: "upload",
+      path: "/videoupload",
+      fun: function () {
+        console.log(this.path);
+        navigate("/main");
+      },
+    },
+    {
+      name: "Guide",
+      path: "/guide",
+      fun: function () {
+        console.log(this.path);
+        navigate("/main");
+      },
+    },
+    {
+      name: "logout",
+      path: "/",
+      fun: function () {
+        console.log(this.path);
+        navigate("/main");
+      },
+    },
   ];
 
   return (
     <div className="Sidebar">
       <img className="profile" src="../image/profile.png" />
-      {menus.map((menu, index) => {
+      {menus.map((menu, i) => (
+        <div className="Sidebar-menu" onClick={menu.fun} key={i}>
+          <p className="sidebar-item">{menu.name}</p>
+        </div>
+      ))}
+      {/* {menus.map((menu, index) => {
         return (
           <NavLink className={"Sidebar-menu"} to={menu.path} key={index}>
             <SidebarItem menu={menu} />
           </NavLink>
         );
-      })}
+      })} */}
     </div>
   );
 };
--- web/frontend/src/apis.js
+++ web/frontend/src/apis.js
@@ -1,17 +1,64 @@
-  import axios from 'axios';
+import axios from "axios";
 
-  const baseUrl = 'http://localhost:3001';
+const baseUrl = "http://localhost:3001";
 
-  const urls = {
-      getMember: `${baseUrl}/auth/getMember`,
-      postMenber: `${baseUrl}/auth/postMenber`,
-      uploadVideo:`${baseUrl}/video/uploadVideo`,
+const urls = {
+  login: `${baseUrl}/auth/login`,
+  register: `${baseUrl}/auth/signup`,
+  logout: `${baseUrl}/auth/logout`,
+  auth: `${baseUrl}/auth/auth`,
+  uploadVideo: `${baseUrl}/video/uploadVideo`,
+};
+
+export const login = async (email, password) => {
+  try {
+    const result = await axios.post(
+      urls.login,
+      {
+        email: email,
+        password: password,
+      },
+      { withCredentials: true }
+    );
+    return result.data;
+  } catch (e) {
+    console.log("error", e);
   }
+};
 
-  export const getMember = async ()=>{
-      const result = await axios.get(urls.getMember,{
-
-      });
-      return result;
+export const signup = async (email, password, username, age) => {
+  console.log("register");
+  try {
+    const result = await axios.post(
+      urls.register,
+      {
+        email: email,
+        passwd: password,
+        name: username,
+        age: age,
+      },
+      { withCredentials: true }
+    );
+    return result.data;
+  } catch (e) {
+    console.log("error", e);
   }
+};
 
+export const auth = async () => {
+  try {
+    const result = await axios.get(urls.auth, { withCredentials: true });
+    return result.data;
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const reuslt = await axios.get(urls.logout, { withCredentials: true });
+    return reuslt.data;
+  } catch (e) {
+    console.log("err", e);
+  }
+};
