523dfd665c52dfbba85311621069d46fba51f40d
han0224 <dbn0224@gmail.com>
2022-06-07 13:36:42 +0900

    video 수정

--- web/frontend/src/apis.js
+++ web/frontend/src/apis.js
@@ -9,6 +9,25 @@ const urls = {
   auth: `${baseUrl}/auth/auth`,
   uploadVideo: `${baseUrl}/video/uploadVideo`,
 };
+// winer: 누가 이겼는지(이름String), opponent: 누구랑 쳤는지(이름String), date: 언제 쳤는지(Date), score: 몇대몇('00:00')
+
+export const uploadVideo = async (winer, opponent, date, score) => {
+  try {
+    const result = await axios.post(
+      urls.uploadVideo,
+      {
+        winer: winer,
+        opponent: opponent,
+        date: date,
+        score: score,
+      },
+      { withCredentials: true }
+    );
+    return result.data;
+  } catch (e) {
+    console.log("error", e);
+  }
+};
 
 export const login = async (email, password) => {
   try {
