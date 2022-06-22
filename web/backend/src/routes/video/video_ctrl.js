f960f3c938f984761c3e8fec1e6d023f18e2f1c3
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-06-23 01:07:40 +0900

    Update video_ctrl.js

--- web/backend/src/routes/video/video_ctrl.js
+++ web/backend/src/routes/video/video_ctrl.js
@@ -3,10 +3,10 @@ const schema = require('../../models/db-schema');
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const bucketName = process.env.BUCKET_NAME;
+const region = process.env.BUCKET_REGION;
+const accessKeyId = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_KEY;
 
 const s3 = new S3({
     region,
