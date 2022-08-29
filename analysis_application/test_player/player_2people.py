980cc525850350a8b72cb079b03befaca4260dfc
1ilkwon <dlfrnjs318@gmail.com>
2022-08-29 23:52:55 +0900

    나머지 경기영상, 선수좌표
    전부 추출

--- analysis_application/test_player/player_2people.py
+++ analysis_application/test_player/player_2people.py
@@ -28,8 +28,8 @@ class Player:
 
     def play_video(self):
         if self.cap.isOpened():
-            player_coord_text = open('../Player_coord/7gamevideo_score4_player.json_coord.json', 'w')
-            rival_coord_text = open('../Player_coord/7gamevideo_score4_rival_coord.json', 'w')
+            player_coord_text = open('../Player_coord/6gamevideo_score2_player.json_coord.json', 'w')
+            rival_coord_text = open('../Player_coord/6gamevideo_score2_rival_coord.json', 'w')
 
             count = 0
             while True:
--- analysis_application/test_player/player_main.py
+++ analysis_application/test_player/player_main.py
@@ -1,4 +1,4 @@
 from player_2people import Player
 
-player = Player("../video/7gamevideo_score1.mp4", "win")
+player = Player("../video/6gamevideo_score2.mp4", "win")
 player.play_video()
