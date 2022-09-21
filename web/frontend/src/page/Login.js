ecb2aac4f7fec75da3bae4f314286fee963f6ebc
han0224 <dbn0224@gmail.com>
2022-09-21 13:09:43 +0900

    login, signup page css

--- web/frontend/src/page/Login.js
+++ web/frontend/src/page/Login.js
@@ -1,15 +1,43 @@
 import "Styles/LoginPage.css";
+import useInput from "Hooks/useInput";
+import { ICON } from "Constants/icons";
 
 const Login = () => {
+  const email = useInput("");
+  const password = useInput("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.value, password.value);
+  };
   return (
     <div className="loginPage">
+      <div className="login-title">
+        <a href="/">
+          <img src={ICON.LOGO} />
+          <h1>A.Te.Co</h1>
+        </a>
+      </div>
       <div className="login-card">
         <div className="login-msg">
           <p>Welcome back!</p>
           <p>Sign in to your accunt</p>
         </div>
-        <div className="login-form">
-          <form></form>
+        <form className="login-form" onSubmit={onSubmit}>
+          <input type={"email"} {...email} placeholder={"Email"} required />
+          <input
+            type={"password"}
+            {...password}
+            placeholder={"Password"}
+            required
+          />
+          <input type={"submit"} value={"Login"} />
+        </form>
+        <div className="login-card-footer">
+          <p>
+            Don`t have an account?<a href="/signup">Sign up</a>
+          </p>
+          <a href="/">Forgot password?</a>
         </div>
       </div>
     </div>
--- web/frontend/src/page/Signup.js
+++ web/frontend/src/page/Signup.js
@@ -1,7 +1,62 @@
+import "Styles/SignupPage.css";
+import useInput from "Hooks/useInput";
+import { ICON } from "Constants/icons";
+
 const Signup = () => {
+  const email = useInput("");
+  const password = useInput("");
+  const checkPw = useInput("");
+  const name = useInput("");
+  const team = useInput("");
+  const weight = useInput("");
+  const height = useInput("");
+  const age = useInput("");
+  const isAgree = useInput(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.value, password.value);
+  };
   return (
-    <div>
-      <div>Signup</div>
+    <div className="signupPage">
+      <div className="signup-title">
+        <a href="/">
+          <img src={ICON.LOGO} />
+          <h1>A.Te.Co</h1>
+        </a>
+      </div>
+      <div className="signup-card">
+        <div className="signup-msg">
+          <p>Create a A.Te.Co Account</p>
+        </div>
+        <form className="signup-form" onSubmit={onSubmit}>
+          <input type={"email"} {...email} placeholder={"Email"} required />
+          <input
+            type={"password"}
+            {...password}
+            placeholder={"Password"}
+            required
+          />
+          <input
+            type={"password"}
+            {...checkPw}
+            placeholder={"Confirm your password"}
+            required
+          />
+          <input type={"string"} {...name} placeholder={"Your name"} required />
+          <input type={"string"} {...team} placeholder={"Team"} required />
+          <input type={"number"} {...weight} placeholder={"Weight"} required />
+          <input type={"number"} {...height} placeholder={"Height"} required />
+          <input type={"number"} {...age} placeholder={"Age"} required />
+
+          <input type={"submit"} value={"Login"} />
+        </form>
+        <div className="signup-card-footer">
+          <p>
+            Already have an account?<a href="/login"> Login</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
