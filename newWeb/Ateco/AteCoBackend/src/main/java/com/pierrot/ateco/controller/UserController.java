b4248bd4db30ba9261578715d5b2cafabf050c14
yongminKang <rkdrnwkd1@gmail.com>
2022-09-23 18:58:19 +0900

    .

---
+++ newWeb/Ateco/AteCoBackend/src/main/java/com/pierrot/ateco/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.pierrot.ateco.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	
+}
