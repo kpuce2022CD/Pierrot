fb8a1083bf1cbae6502c8e2716232aa328fd2818
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-10-27 22:25:18 +0900

    스프링부트기초설정

---
+++ newWeb/tennisAnalysis/src/main/java/com/pierrot/tennisAnalysis/TennisAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.pierrot.tennisAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisAnalysisApplication.class, args);
+	}
+
+}
---
+++ newWeb/tennisAnalysis/src/test/java/com/pierrot/tennisAnalysis/TennisAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierrot.tennisAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
