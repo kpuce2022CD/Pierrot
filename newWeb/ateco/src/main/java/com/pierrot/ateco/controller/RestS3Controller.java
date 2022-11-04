d36c71ff34ef34c232160d8d75796a92242105ac
1ilkwon <dlfrnjs318@gmail.com>
2022-11-04 10:53:48 +0900

    123

--- newWeb/ateco/src/main/java/com/pierrot/ateco/controller/RestS3Controller.java
+++ newWeb/ateco/src/main/java/com/pierrot/ateco/controller/RestS3Controller.java
@@ -2,11 +2,9 @@ package com.pierrot.ateco.controller;
 
 import com.pierrot.ateco.service.AmazonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -29,11 +27,6 @@ public class RestS3Controller {
     @GetMapping("/getVideoList")
     public List<String> getFileList() {
         return this.amazonService.listFiles();
-    }
-
-    @GetMapping("/csv_download/{fileName}")
-    public ResponseEntity<byte[]> download(@PathVariable String fileName) throws IOException {
-        return this.amazonService.getObject(fileName);
     }
 
 }
