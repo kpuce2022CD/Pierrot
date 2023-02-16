382cef5e262fef79186d5256bb76d137e83b75a4
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-02-17 03:04:33 +0900

    문서 삭제 및 데모영상

--- newWeb/ateco/src/main/java/com/pierrot/ateco/repository/GameInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pierrot.ateco.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.pierrot.ateco.domain.GameInfoDto;
-
-public interface GameInfoRepository extends MongoRepository<GameInfoDto,String>{
-
-}
